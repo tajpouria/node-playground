afda39388a87a3d151334152bc555a63fa18ed65
tajpouria <tajpouria.dev@gmail.com>
2020-09-13 23:20:17 +0430

    Added -- h2 node intro

--- h2/intro/http2.js
+++ h2/intro/http2.js
@@ -2,58 +2,38 @@ const fs = require("fs");
 const path = require("path");
 const mime = require("mime");
 const http2 = require("http2");
+const http = require("http");
 
 const files = getFiles();
 
 let port = NaN;
-http2.createSecureServer(
-  {
-    key: fs.readFileSync(path.join(__dirname, ".secret/key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, ".secret/cert.pem")),
-  },
-  (req, res) => {
-    const file = files.get(
-      Array.from(files.keys())[Math.floor(Math.random() * files.size)],
-    );
+http2
+  .createSecureServer(
+    {
+      key: fs.readFileSync(path.join(__dirname, ".secret/key.pem")),
+      cert: fs.readFileSync(path.join(__dirname, ".secret/cert.pem")),
+    },
+    (req, res) => {
+      const {
+        HTTP2_HEADER_CONTENT_LENGTH,
+        HTTP2_HEADER_LAST_MODIFIED,
+        HTTP2_HEADER_STATUS,
+        HTTP2_HEADER_CONTENT_TYPE,
+      } = http2.constants;
 
-    res.stream.pushStream(
-      { [http2.constants.HTTP2_HEADER_PATH]: file.filepath },
-      (err, pushStream) => {
-        if (err) {
-          console.error(err);
-          res.writeHead(500);
-          res.end("Internal Server Error");
-        }
-        pushStream.respondWithFD(file.fileDesc, file.headers);
-      },
-    );
-    res.stream.respond({ ":status": 200 });
-  },
-);
+      const fileKey = Array.from(files.keys())[
+        Math.floor(Math.random() * files.size)
+      ];
+      const file = files.get(fileKey);
 
-const server = http2.createSecureServer({
-  key: fs.readFileSync(path.join(__dirname, ".secret/key.pem")),
-  cert: fs.readFileSync(path.join(__dirname, ".secret/cert.pem")),
-});
-server
-  .on("stream", (stream, headers, flags) => {
-    const {
-      HTTP2_HEADER_METHOD,
-      HTTP2_HEADER_PATH,
-      HTTP2_HEADER_STATUS,
-      HTTP2_HEADER_CONTENT_TYPE,
-    } = http2.constants;
-
-    const method = headers[HTTP2_HEADER_METHOD];
-    const path = headers[HTTP2_HEADER_PATH];
-
-    stream.respond({
-      [HTTP2_HEADER_STATUS]: 200,
-      [HTTP2_HEADER_CONTENT_TYPE]: "text/plain; charset=utf-8",
-    });
-    stream.write("hello ");
-    stream.end("world");
-  })
+      res.stream.respondWithFD(file.fileDesc, {
+        [HTTP2_HEADER_STATUS]: 200,
+        [HTTP2_HEADER_CONTENT_LENGTH]: file.contentLength,
+        [HTTP2_HEADER_CONTENT_TYPE]: file.contentType,
+        [HTTP2_HEADER_LAST_MODIFIED]: file.lastModified,
+      });
+    },
+  )
   .listen((port = 3000), "127.0.0.1", () =>
     console.info(`Listening on port ${port} ...`),
   );
@@ -67,17 +47,23 @@ function getFiles(dir = "/home/tajpouria/Pictures") {
     const stats = fs.fstatSync(fileDesc);
     const contentType = mime.getType(filepath);
 
-    if (contentType?.match(/(.*.(jpg|jpeg|png|webp|svg)$)/))
+    if (contentType?.match(/(.*.(jpg|jpeg|png)$)/))
       files.set("/" + fileName, {
         filepath,
         fileDesc,
-        headers: {
-          "Content-Length": stats.size,
-          "Last-Modified": stats.mtime.toUTCString(),
-          "Content-Type": contentType,
-        },
+        contentType,
+        contentLength: stats.size,
+        lastModified: stats.mtime.toUTCString(),
       });
   });
 
   return files;
 }
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200);
+    res.setHeader("Content-Type", "text/html");
+    res.end("Hello World!");
+  })
+  .listen(3001, "127.0.0.1", () => console.info(`Listening on Port ${port}`));
