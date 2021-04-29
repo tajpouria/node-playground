025a42889334f2be686a08bfa117a0dea52ce40f
tajpouria.dev@gmail.com <tajpouria.dev@gmail.com>
2021-04-29 18:52:50 +0430

    Added -- Usage mig

--- dive-into-mongo/product-sps-migration.js
+++ dive-into-mongo/product-sps-migration.js
@@ -1,3 +1,11 @@
+/**
+ * Install MongoDB shell:
+ * @link https://askubuntu.com/a/1127143
+ *
+ * usage:
+ * > mongo mongodb+srv://{user}:{password}@{host}/${request db name} ./product-sps-migration.js
+ */
+
 const PRODUCT_DB_NAME = "product";
 
 const productDB = db.getSiblingDB(PRODUCT_DB_NAME);
